17a7c3869cd7acbdc83f3333a30b0c659b359e4b
Devendra Singh <devsinghindra@gmail.com>
2020-04-25 19:37:45 +0530

    added build for covid19

---
+++ build/precache-manifest.bc9582c0daf9a517728451e6eb8e2e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c49e19d3ccdeb21f4439a0c02b8f24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8ce737b9f4c029406d",
+    "url": "/static/css/main.9a276d6e.chunk.css"
+  },
+  {
+    "revision": "ee72be7254aab8b53042",
+    "url": "/static/js/2.dc725e96.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.dc725e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8ce737b9f4c029406d",
+    "url": "/static/js/main.e160bc67.chunk.js"
+  },
+  {
+    "revision": "d04fac08b2c8611c842e",
+    "url": "/static/js/runtime-main.7a4e2425.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server is listening at port 3000");
+});
